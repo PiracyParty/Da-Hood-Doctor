2247fc79fa7a3a5c81369790e0cf8a5eb129ad51
Piracy <83539614+PiracyParty@users.noreply.github.com>
2024-09-20 23:10:54 +0100

    Update ActualScript.lua
    fixed it i added .lua lol

--- ActualScript.lua
+++ ActualScript.lua
@@ -1,4 +1,4 @@
-local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/PiracyParty/Da-Hood-Doctor/main/GUI"))();
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/PiracyParty/Da-Hood-Doctor/main/GUI.lua"))();
 
 local Gamer = library:CreateSection("Safehouses");
 
